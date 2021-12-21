af8195b1bd9de809c88d1ff32171edfa88e1f82a
Pavel Hrách <pavel.hrach@moderntv.eu>
2021-12-21 13:20:43 +0100

    finished day3

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -3,35 +3,41 @@ const INPUT: &'static str = include_str!("../../input/day03.txt");
 const INPUT_SIZE: u32 = 12;
 
 fn main() {
-    let mut zeroes = [0u32; INPUT_SIZE as usize];
-    let mut ones = [0u32; INPUT_SIZE as usize];
+    let numbers: Vec<u32> = INPUT
+        .lines()
+        .map(|l| u32::from_str_radix(l, 2).unwrap())
+        .collect();
 
-    for l in INPUT.lines() {
-        let n = u32::from_str_radix(l, 2).unwrap();
+    let gamma = (0..INPUT_SIZE).fold(0, |gamma, i| gamma | most_common_bit(&numbers, i) << i);
+    let epsilon = gamma ^ !(u32::MAX << INPUT_SIZE);
 
-        for i in 0..INPUT_SIZE {
-            if n & 2u32.pow(i) > 0 {
-                ones[i as usize] += 1;
-            } else {
-                zeroes[i as usize] += 1;
-            }
+    println!("{}", gamma * epsilon);
+
+    let mut oxygens = numbers.clone();
+    for i in (0..INPUT_SIZE).rev() {
+        if oxygens.len() == 1 {
+            break;
         }
+        let bit = most_common_bit(&oxygens, i);
+        oxygens.retain(|n| (n >> i) & 1 == bit);
     }
 
-    let mut gamma = 0;
-    let mut epsilon = 0;
-
-    for i in 0..INPUT_SIZE {
-        let bit = 2u32.pow(i);
-        if ones[i as usize] > zeroes[i as usize] {
-            gamma += bit;
-        } else {
-            epsilon += bit;
+    let mut co2s = numbers;
+    for i in (0..INPUT_SIZE).rev() {
+        if co2s.len() == 1 {
+            break;
         }
+        let bit = most_common_bit(&co2s, i) ^ 1;
+        co2s.retain(|n| (n >> i) & 1 == bit);
     }
 
-    println!("{:012b}", gamma);
-    println!("{:012b}", epsilon);
+    match (oxygens.as_slice(), co2s.as_slice()) {
+        ([oxygen], [co2]) => println!("{}", oxygen * co2),
+        _ => panic!("expected to filter out single candidate instead got, oxygen: {}, co2: {}", oxygens.len(), co2s.len()),
+    }
+}
 
-    println!("{}", gamma * epsilon)
+fn most_common_bit(numbers: &[u32], i: u32) -> u32 {
+    let ones = numbers.iter().fold(0, |ones, n| ones + ((n >> i) & 1));
+    (ones * 2 >= numbers.len() as u32) as u32
 }
